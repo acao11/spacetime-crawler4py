45e164139028c06199abd6a92cf072f0ef52c0c6
Akki2004 <36495akki@gmail.com>
2026-05-01 16:33:31 -0700

    updated calendar trap

--- scraper.py
+++ scraper.py
@@ -68,6 +68,9 @@ def is_valid(url):
         if re.search(r"/day/\d{4}-\d{2}-\d{2}", parsed.path.lower()):
             return False
 
+        if re.search(r"/\d{4}-\d{2}(/|$)", parsed.path.lower()):
+            return False
+
         # 2. Repeating Directory Pattern
         # Some traps look like /news/news/news/news/... 
         # We split the path by '/' and check if any folder name appears more than twice.
